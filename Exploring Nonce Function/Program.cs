ecec03ec8b8a2399f3180c98f0c29f0aea675016
ByatisGlaaki <rares_famuntean@yahoo.com>
2025-11-04 12:42:27 +0200

    Add project files.

---
+++ Exploring Nonce Function/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading.Channels;
+using ExploringNonceFunction.Core;
+
+int id = 0;
+
+string data = "Tranzactie: Rares -> Vlazi";
+Block block = new Block(id++, "00000000000000000000000000", data);
+
+Console.WriteLine("User 1 Mines block 1...");
+block.Mine();
+Console.WriteLine($"Block Minat: {block.Hash}");
+Console.WriteLine($"Nonce folosit: {block.Nonce}\n");
+
+Block block2 = new Block(id++, block.Hash, block.Data);
+Console.WriteLine("User 2 Mines block 2...");
+block2.Mine();
+Console.WriteLine($"Block Minat: {block2.Hash}");
+Console.WriteLine($"Nonce folosit: {block2.Nonce}\n");
---
+++ exploring-nonce-function.Core/Block.cs
@@ -0,0 +1,55 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace ExploringNonceFunction.Core
+{
+    public class Block
+    {
+        public int Index { get; private set; }
+        public string Data { get; private set; } = string.Empty; // Contentul blocului
+        public long Nonce { get; private set; } = 0;
+        public int Difficulty { get; set; } // Cu cat e mai mare numarul cu atat e mai greu de minat (numarul este mai mic)
+        public string PreviousHash { get; set; } = string.Empty; // Hashul blocului anterior
+        public string Hash { get; private set; } = string.Empty;
+        public DateTime TimeStamp { get; private set; } // Cand s a create blocul
+        
+        public Block(int index, string previousHash, string data, int difficulty = 10)
+        {
+            Index = index;
+            PreviousHash = previousHash;
+            Data = data;
+            Difficulty = difficulty;
+            TimeStamp = DateTime.UtcNow;
+        }
+
+        private string CalculateHash()
+        {
+            using (SHA256 sha256 = SHA256.Create())
+            {
+                string rawData = $"{Index}{PreviousHash}{Data}{Nonce}{TimeStamp:O}";
+                byte[] bytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(rawData));
+                StringBuilder builder = new StringBuilder();
+
+                foreach (byte b in bytes)                
+                    builder.Append(b.ToString("x2"));
+                
+                return builder.ToString();
+            }
+        }
+
+        public void Mine()
+        {
+            string targetZeroes = new string('0', Difficulty);
+            while (true)
+            {
+                Hash = CalculateHash();
+                if (Hash.StartsWith(targetZeroes))
+                    break;
+                Nonce++;
+            }
+        }
+
+    }
+}
